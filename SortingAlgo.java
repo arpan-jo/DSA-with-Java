870a4d1dd851d6f32ebd1075c57d033be46f2a39
arpan-jo <arpanjorng@gmail.com>
2023-05-19 22:00:37 +0600

    insertion sort added

--- SortingAlgo.java
+++ SortingAlgo.java
@@ -1,5 +1,9 @@
 public class SortingAlgo {
 
+    // bubble sort, selection sort, insertion sort ===> O(n2)
+    // bobble sort ===> incresing order
+    // selection sort ===> decreasing order
+
     public static void bublleSort(int arr[]) {
         for (int turn = 0; turn < arr.length - 1; turn++) {
             boolean swap = false;
@@ -16,13 +20,6 @@ public class SortingAlgo {
         }
     }
 
-    public static void printArr(int arr[]) {
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
-        }
-        System.out.println();
-    }
-
     public static void selectionSort(int arr[]) {
 
         for (int i = 1; i < arr.length - 1; i++) {
@@ -32,6 +29,7 @@ public class SortingAlgo {
                     minPosition = j;
                 }
             }
+            // swap
             int temp = arr[minPosition];
             arr[minPosition] = arr[i];
             arr[i] = temp;
@@ -39,12 +37,35 @@ public class SortingAlgo {
 
     }
 
+    public static void insertionSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int current = arr[i];
+            int previous = i - 1;
+
+            // finding out the current position to insert
+            while (previous >= 0 && arr[previous] > current) {
+                arr[previous + 1] = arr[previous];
+                previous--;
+            }
+
+            // insertion
+            arr[previous + 1] = current;
+        }
+
+    }
+
+    public static void printArr(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
     public static void main(String[] args) {
         int arr[] = { 5, 3, 4, 1, 2 };
-        bublleSort(arr);
-        // printArr(arr);
+        // bublleSort(arr);
         // selectionSort(arr);
+        insertionSort(arr);
         printArr(arr);
-        // System.out.println(arr.length);
     }
 }
